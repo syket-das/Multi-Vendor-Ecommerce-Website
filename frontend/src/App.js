02a91ee3372404825d9192b69a2ce47f35e10bc0
Syket Das <80612737+syket-das@users.noreply.github.com>
2022-02-04 14:04:03 +0530

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Amazon</a>
+      </header>
+      <main>
+        list Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
