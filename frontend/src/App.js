38c981ad26dcf4c37d61a1af9cff019f836097c9
Syket Das <80612737+syket-das@users.noreply.github.com>
2022-02-15 12:41:35 +0530

    admin action done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SigninScreen from './screens/SigninScreen';
+import UserEditScreen from './screens/UserEditScreen';
 import UserListScreen from './screens/UserListScreen';
 
 function App() {
@@ -118,6 +119,10 @@ function App() {
             component={OrderListScreen}
           ></AdminRoute>
           <AdminRoute path="/userlist" component={UserListScreen}></AdminRoute>
+          <AdminRoute
+            path="/user/:id/edit"
+            component={UserEditScreen}
+          ></AdminRoute>
 
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
